27c789cf49923a59e54708fa9c209ea281ff41f0
Muhammad Rais Adlani <39582869+mraisadlani@users.noreply.github.com>
2021-02-02 16:47:38 +0700

    Entities Product

---
+++ api/controller/ProductController.go
@@ -0,0 +1,23 @@
+package controller
+
+import "github.com/gin-gonic/gin"
+
+func GetAllProduct(c *gin.Context) {
+
+}
+
+func FindProduct(c *gin.Context) {
+
+}
+
+func CreateProduct(c *gin.Context) {
+
+}
+
+func UpdateProduct(c *gin.Context) {
+
+}
+
+func DeleteProduct(c *gin.Context) {
+
+}
